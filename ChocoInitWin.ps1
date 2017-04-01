63696a84f6ee1eeb8efa948b22f7f8b245fe67cb
Merrick D <merrickd@gmail.com>
2017-04-01 02:15:12 -0700

    Add more software

--- ChocoInitWin.ps1
+++ ChocoInitWin.ps1
@@ -1,7 +1,7 @@
 # ChocoInitWin.ps1
 # A simple Powershell script to make setting up my Windows 10 environment quicker.
-
-# ExecutionPolicy must not be Restricted.  Check via Get-ExecutionPolicy and set via Set-ExecutionPolicy Bypass
+# ExecutionPolicy must not be Restricted.
+# Check via 'Get-ExecutionPolicy' and set via 'Set-ExecutionPolicy Bypass'
 
 # Install Chocolatey
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
@@ -14,8 +14,9 @@ if ($? == False) {
 # Install packages with Chocolatey
 Write-Host "Installing packages..."
 
-# Considering supressing output here and possibly adding a progress bar
+# Consider supressing output here and possibly adding a progress bar
 
+cinst GeekUninstaller -y
 cinst putty -y
 cinst peazip -y
 cinst git -y
@@ -26,6 +27,10 @@ cinst malwarebytes -y
 cinst slack -y
 cinst sysinternals -y
 cinst xming -y
+cinst spacesniffer -y
+cinst haroopad -y
+cinst thunderbird -y
+cinst libreoffice -y
 cinst vcredist2010 -y
 cinst vcredist2012 -y
 cinst vcredist2013 -y
